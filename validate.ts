fde13b294ce699531a069c3a641ce8fb7e8ab2b3
multivit4min <david.kartnaller@gmail.com>
2020-10-02 09:38:50 +0000

    add showcase plugin

--- validate.ts
+++ validate.ts
@@ -1,6 +1,17 @@
 import yaml from "yaml"
 import { promises as fs } from "fs"
 import Joi from "joi"
+import fetch from "node-fetch"
+
+declare interface Configuration {
+  plugins: {
+    name: string
+    description: string
+    version: string
+    repository: string
+    commit: string
+  }[]
+}
 
 const schema = Joi.object({
   plugins: Joi.array().items(Joi.object({
@@ -15,7 +26,16 @@ const schema = Joi.object({
 fs.readFile("./repository.yaml", "utf-8")
   .then(data => yaml.parse(data))
   .then(data => schema.validateAsync(data))
-  .then(data => console.log(data))
+  .then(({ plugins }: Configuration) => {
+    return Promise.all(plugins.map(async plugin => {
+      const url = `${plugin.repository}/archive/${plugin.commit}.zip`
+      console.log(`fetching HEAD "${url}" ...`)
+      const res = await fetch(url, { method: "HEAD" })
+      if (res.status === 200) return
+      throw new Error(`received non 200 status code for plugin "${plugin.name}" (${res.status})`)
+    }))
+  })
+  .then(() => console.log("validation passed"))
   .catch(e => {
     console.error(e)
     process.exit(1)
